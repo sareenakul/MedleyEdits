58244471b128ef2bf6cdd3e862076bec972ce770
Akul Sareen <sareen.akul@gmail.com>
2024-02-21 00:00:41 -0500

    Adding toggle button

--- medleytutors/src/Components/Toggle.jsx
+++ medleytutors/src/Components/Toggle.jsx
@@ -3,7 +3,14 @@ import React from "react";
 const Toggle = ({ onToggle, label }) => {
   return (
     <div className="toggle">
-      <button onClick={onToggle}>{label}</button>
+      <div className="notch"/>
+      <div>
+        <div className="shape sm"/>
+        <div className="shape sm"/>
+        <div className="shape md"/>
+        <div className="shape lg"/>
+      </div>
+      <button onClick={onToggle}></button>
     </div>
   );
 };
